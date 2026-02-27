a73282e0c6a01f4b4a778b11b0e0f039d88ab22a
Anton K <drvirus15@gmail.com>
2026-02-27 18:51:13 +0200

    some new changes

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -3,7 +3,14 @@ import Bull from "./Bull";
 const Matador = () =>
     <div><img src="../src/assets/matador.png" style={{ width: '150px', height: '200px' }}
         alt="matador"></img></div>
-const BullRun = document.addEventListener('bullRun', (event)=> {
+
+const BullRun = document.addEventListener('bullRun', (event) => {
+    const pos = event as CustomEvent;
+    let bullPosition = pos.detail.position;
     
+    if(bullPosition === 2){
+        
+        console.log("we have to get out of here!", Matador);
+    }
 });
 export { Matador };

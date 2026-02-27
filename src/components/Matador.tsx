96dc0e3ea1f51a4ffcc32e465cc87e5662017a40
Lenovo <drvirus15@gmail.com>
2026-02-27 15:48:33 +0200

    some changes

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,1 +1,9 @@
-export const Matador = () => <div>i am matador</div>
+import Bull from "./Bull";
+
+const Matador = () =>
+    <div><img src="../src/assets/matador.png" style={{ width: '150px', height: '200px' }}
+        alt="matador"></img></div>
+const BullRun = document.addEventListener('bullRun', (event)=> {
+    
+});
+export { Matador };
